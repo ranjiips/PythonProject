6d0db48fe1b7bf5b9220efe6c8fd0d485fcdec67
ranjiips <ranji_ips@yahoo.com>
2024-07-03 22:15:12 +0530

    Updated into GIT

---
+++ PyTest/test_case_demo1.py
@@ -0,0 +1,16 @@
+import pytest
+
+@pytest.yield_fixture()
+def setup():
+    print("\nSetup - Execute before each test methods")
+    yield
+    print("Tear Down - Execute after each test methods")
+
+def test_methodA(setup):
+    print("\nExecuting the test case A")
+
+def test_methodB(setup):
+    print("\nExecuting the test case B")
+
+def test_methodC(setup):
+    print("\nExecuting the test case C")
