2243b0db8023034d0f04548f309d678efa2ba93a
ranjiips <ranji_ips@yahoo.com>
2024-07-10 23:15:33 +0530

    Updated the changes and push to GIT

---
+++ ProblemSolving/HackerRank/Implementation.py
@@ -0,0 +1,23 @@
+import unittest
+import numpy as np
+from datetime import datetime
+
+class TestImplementationFunctions(unittest.TestCase):
+
+    # refer to the below page for the problem desciptions
+    # https://www.hackerrank.com/domains/algorithms?filters%5Bsubdomains%5D%5B%5D=implementation
+    def test_gradingStudents(self):
+        print("Logic to implement the grading students")
+
+    def test_apple_orange(self):
+        print("Logic to implement the apple and orange")
+
+    def test_number_line_jumps(self):
+        print("Logic to implement the number line jumps")
+
+    def test_between_two_sets(self):
+        print("Logic to implement the between two sets")
+
+    def test_breaking_the_records(self):
+        print("Logic to implement the breaking the records")
+
--- ProblemSolving/HackerRank/Warmup.py
+++ ProblemSolving/HackerRank/Warmup.py
@@ -1,8 +1,11 @@
 import unittest
 import numpy as np
+from datetime import datetime
 
 class TestWarmupFunctions(unittest.TestCase):
 
+    # refer to the below page for the problem desciptions
+    # https://www.hackerrank.com/domains/algorithms?filters%5Bsubdomains%5D%5B%5D=warmup
     def test_AddTwoNumbers(self):
         a=5
         b=6
@@ -42,4 +45,57 @@ class TestWarmupFunctions(unittest.TestCase):
             i=i+1
         print(abs(leftDiagonal-rightDiagonal))
 
-    # def test_plus_minus_ratio:
+    def test_plus_minus_ration(self):
+        arr = [-4, 3, -9, 0, 4, 1]
+        positiveNumCount = 0
+        negativeNumCount = 0
+        zeroCount = 0
+
+        for i in arr:
+            if i>0:
+                positiveNumCount = positiveNumCount+1
+            elif i<0:
+                negativeNumCount = negativeNumCount+1
+            elif i==0:
+                zeroCount = zeroCount+1
+        posRatio = "{:.5f}".format(positiveNumCount/len(arr))
+        negRatio = "{:.5f}".format(negativeNumCount/len(arr))
+        zerRatio = "{:.5f}".format(zeroCount/len(arr))
+        print(f"Positive number count {positiveNumCount} and the ratio is {posRatio}")
+        print(f"Negative number count {negativeNumCount} and the ratio is {negRatio}")
+        print(f"Zero count {zeroCount} and the ratio is {zerRatio}")
+
+    def test_staircase(self):
+        n=4
+        for i in range(0,n):
+            print(" ")
+            for j in range(0,n):
+                if j>=n-1-i:
+                    print("#", end ="")
+                else:
+                    print(" ", end ="")
+
+    def test_staircases(self,num_stairs=4):
+        for stairs in range(1, num_stairs + 1):
+            print(' ' * (num_stairs - stairs) + '#' * stairs)
+
+    def test_minmaxsum(self):
+        arr=[1,3,5,7,9]
+        minArr = arr.copy()
+        maxArr = arr.copy()
+        minimum = min(arr)
+        maximum = max(arr)
+        maxArr.remove(minimum)
+        minArr.remove(maximum)
+        print(sum(minArr), sum(maxArr))
+
+    def test_birthday_cake_candles(self):
+        candles = [4,4,1,3]
+        maximum = max(candles)
+        maxOccurance = len([i for i in candles if i==maximum])
+        print(maxOccurance)
+
+    def test_timeConversion(self):
+        s = '07:05:45PM'
+        t = datetime.strptime(s,'%I:%M:%S%p')
+        print(t.strftime('%H:%M:%S'))
